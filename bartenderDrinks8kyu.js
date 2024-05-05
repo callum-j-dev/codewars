41fb963bafb47433747053ac18d6d52ca35bebe9
Callum Novak <callum.j.novak@gmail.com>
2024-05-05 18:31:19 -0400

    added 8kyu solution for 5/5

---
+++ bartenderDrinks8kyu.js
@@ -0,0 +1,27 @@
+function getDrinkByProfession(param){
+    const person = param.toLowerCase();
+    switch (person) {
+        case 'jabroni':
+          return 'Patron Tequila';
+          break;
+        case 'school counselor':
+          return 'Anything with Alcohol';
+          break;
+        case 'programmer':
+          return 'Hipster Craft Beer';
+          break;
+        case 'bike gang member':
+          return 'Moonshine';
+          break;
+        case 'politician':
+          return 'Your tax dollars';
+          break;
+        case 'rapper':
+          return 'Cristal';
+          break;
+        default:
+          return 'Beer';
+          break;
+    }
+  
+  }
